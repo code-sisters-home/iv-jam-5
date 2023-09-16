31c6bc6311d3ff40814aad361c3f29c613a8a1e5
A K <frecklesmobile@gmail.com>
2023-09-17 02:56:17 +0300

    lightning update

--- mobius_bike/Assets/Scripts/ThrowableObject.cs
+++ mobius_bike/Assets/Scripts/ThrowableObject.cs
@@ -4,22 +4,46 @@ using UnityEngine;
 
 public class ThrowableObject : MonoBehaviour
 {
-	  private float nextTime = 0.0f;
+    public GameObject targetObject;	
+	private float nextTime = 0.0f;
     public float period = 0.09f;
+	public float speed = 10.0f;
+	
 	private Space initSpace;
+	private Transform target;
     // Start is called before the first frame update
     void Start()
     {
-        initSpace = Space.Self;
+		target = targetObject.transform;
+		transform.LookAt(target.position);	
+		initSpace = Space.Self;
     }
 
     // Update is called once per frame
     void Update()
     {
+		if(target != targetObject.transform)
+		{	
+			target = targetObject.transform;
+			transform.LookAt(target.position);	
+			initSpace = Space.Self;				
+		}		
+		
         if (Time.time > nextTime)
         {
             nextTime = Time.time + period;
-			transform.Rotate(0, Random.Range(0.0f, 180.0f), 0, initSpace);
-		}
+			transform.Rotate(0, 0, Random.Range(0.0f, 180.0f), initSpace);
+		}		
+		
+	    var step =  speed * Time.deltaTime; // calculate distance to move
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);	
+
+
+	 if (Vector3.Distance(transform.position, target.position) < 0.001f)
+        {
+            // Swap the position of the cylinder.
+            Destroy(gameObject);
+        }		
+		
     }
 }
--- mobius_bike/Assets/Scripts/Thunder.cs
+++ mobius_bike/Assets/Scripts/Thunder.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class Thunder : MonoBehaviour
 {
-    private float nextActionTime = 0.0f;
-    public float period = 5f;
+    public GameObject lightningPrefab;
+	public GameObject targetObject;
+	private float nextSpawnTime = 0.0f;
+	private float nextEvilnessTime = 0.0f;
+    public float lightningPeriod = 2f;
+	public float evilnessPeriod = 20f;
+	public float evilnessTime = 5f;
 
     // Start is called before the first frame update
     void Start()
@@ -16,11 +21,26 @@ public class Thunder : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Time.time > nextActionTime)
-        {
-            nextActionTime = Time.time + period;
+		if(Time.time > nextEvilnessTime && Time.time < nextEvilnessTime + evilnessTime)
+		{
+			float evilness = 1 - Mathf.Abs((Time.time - nextEvilnessTime)/evilnessTime - 0.5f)*2.0f;
+			float k = Mathf.Lerp(1.0f, 0.3f, evilness);
+			GetComponent<Renderer>().material.SetColor("_BaseColor", new Color(k, k, k, 1));
+			
+			if (Time.time > nextSpawnTime)
+			{
+				nextSpawnTime = Time.time + lightningPeriod;
 
-            //GameObject lightningObject = Instantiate(lightning, transform.position, Quaternion.identity) as GameObject;
-        }
+				GameObject lightningObject = Instantiate(lightningPrefab, transform.position, Quaternion.identity) as GameObject;
+				lightningObject.AddComponent<ThrowableObject>();
+				lightningObject.GetComponent<ThrowableObject>().targetObject = targetObject;
+				
+			}
+		}
+		if(Time.time > nextEvilnessTime + evilnessTime)
+		{
+			GetComponent<Renderer>().material.SetColor("_BaseColor", new Color(1, 1, 1, 1));
+			nextEvilnessTime = Time.time + evilnessPeriod;
+		}
     }
 }

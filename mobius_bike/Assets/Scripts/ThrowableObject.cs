7f70f342b17b7a1140ca20a927004ddd711f15fa
A K <frecklesmobile@gmail.com>
2023-09-20 21:51:38 +0300

    lightning update

--- mobius_bike/Assets/Scripts/ThrowableObject.cs
+++ mobius_bike/Assets/Scripts/ThrowableObject.cs
@@ -8,26 +8,31 @@ public class ThrowableObject : MonoBehaviour
 	private float nextTime = 0.0f;
     public float period = 0.09f;
 	public float speed = 10.0f;
-	
+	public float lifetime = 20.0f;
 	private Space initSpace;
 	private Transform target;
+	private Vector3 targetPos;
     // Start is called before the first frame update
     void Start()
     {
 		target = targetObject.transform;
+		targetPos = target.position;
 		transform.LookAt(target.position);	
 		initSpace = Space.Self;
+		
+		Destroy(gameObject, lifetime);
     }
 
     // Update is called once per frame
     void Update()
     {
-		if(target != targetObject.transform)
+		/*if(target != targetObject.transform)
 		{	
 			target = targetObject.transform;
 			transform.LookAt(target.position);	
 			initSpace = Space.Self;				
 		}		
+		*/
 		
         if (Time.time > nextTime)
         {
@@ -36,10 +41,10 @@ public class ThrowableObject : MonoBehaviour
 		}		
 		
 	    var step =  speed * Time.deltaTime; // calculate distance to move
-        transform.position = Vector3.MoveTowards(transform.position, target.position, step);	
+        transform.position = Vector3.MoveTowards(transform.position, targetPos, step);	
 
 
-	 if (Vector3.Distance(transform.position, target.position) < 0.001f)
+	 if (Vector3.Distance(transform.position, target.position) < 0.001f || Vector3.Distance(transform.position, targetPos) < 0.001f)
         {
             // Swap the position of the cylinder.
             Destroy(gameObject);

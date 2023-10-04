b26c20ced3b828becf349c9eea8d00516affba5e
A K <frecklesmobile@gmail.com>
2023-10-04 19:44:34 +0300

    mushroom spawner

---
+++ mobius_bike/Assets/Scripts/Spawner.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private CatmullRomSpline path;
+    [SerializeField] private GameObject[] items;
+	public float dropPeriod = 2;
+    public string tag = "drop";	
+	public float roadWidth = 5;
+	
+    private void Start()
+	{
+		//foreach (var item in items)
+		//{
+		//	item.tag = tag;
+		//	SphereCollider sc = item.AddComponent<SphereCollider>();
+		//	sc.isTrigger = true;
+		//}
+		
+	}
+
+    public void DropSomething()
+    {
+        //max is exclusive
+		PositionAndRotation pr = new PositionAndRotation();
+		float s = Random.Range(0.0f, path.controlPointsList.Length);
+        pr = path.GetPositionAndRotation(s);
+		
+		int i = Random.Range(0, items.Length);
+        var item = Instantiate(items[i], pr.position, pr.rotation, transform);
+
+		Vector3 lr = new Vector3(Random.Range(0.0f, 2.0f)-1, 0, 0)*roadWidth;
+		item.transform.position += item.transform.TransformDirection(lr);
+
+		float scale = 2;
+		item.transform.localScale = new Vector3(scale, scale, scale);
+		
+		item.tag = tag;
+		SphereCollider sc = item.AddComponent<SphereCollider>();
+		sc.isTrigger = true;	
+		
+    }
+
+    float timer = 0.0f;
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= dropPeriod)
+        {
+            DropSomething();
+            timer = 0;
+        }
+    }
+}

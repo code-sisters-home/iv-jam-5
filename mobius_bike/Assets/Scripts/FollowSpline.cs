aaa29398f5bd59e2d13feb64fc7a7c43a865cab5
A K <frecklesmobile@gmail.com>
2023-10-05 16:55:46 +0300

    speed fix

--- mobius_bike/Assets/Scripts/FollowSpline.cs
+++ mobius_bike/Assets/Scripts/FollowSpline.cs
@@ -14,20 +14,14 @@ public class FollowSpline : MonoBehaviour
 	public bool useForward = false;
 	
 	public float roadWidth = 5f;
-	public float accStep = 0.1f;
+	public float accStep = 0.01f;
 
 	float shift = 0;
 	float dist = 0;
 	float acc = 0;
-	//private IEnumerator<Transform> pointInPath;
 
 	void OnDrawGizmos()
 	{
-		int index = 0;
-		float t = 0;	
-		//Vector3 pos = path.GetPosition(transform, ref index, ref t);
-		//Gizmos.color = Color.cyan;
-		//Gizmos.DrawLine(transform.position, pos);
 		Gizmos.color = Color.yellow;
 		Gizmos.DrawRay(transform.position, transform.TransformDirection(Vector3.forward).normalized*5.0f);
 		

101e55da3ad2d42a163f3aff27cd4231cbc0e8a5
Janna <jeannette.drobina@gmail.com>
2024-02-25 21:46:30 +0100

    fix save, add authorization

--- mobius_bike/Assets/UI/GameMaster.cs
+++ mobius_bike/Assets/UI/GameMaster.cs
@@ -1,3 +1,8 @@
+﻿using CodeSisters.Logger;
+using InstantGamesBridge;
+using InstantGamesBridge.Modules.Platform;
+using InstantGamesBridge.Modules.Player;
+using System;
 using System.Collections;
 using UnityEngine;
 
@@ -42,12 +47,110 @@ public class GameMaster : MonoBehaviour
         AudioManager = GetComponentInChildren<AudioManager>();
         AudioManager.Init();
 
-        IsReady = true;
+        
         ChangeState(GameState.menu);
-        yield return new WaitForSeconds(1);
+        //yield return new WaitForSeconds(1);
         Loading.FadeOut();
+
+        Bridge.platform.SendMessage(PlatformMessage.GameReady);
+        UnityLogger.Log($"GameReadyим п {DateTime.UtcNow}");
+
+        //если будем релизиться не на яндекс, то вставить проверку Bridge.player.isAuthorizationSupported
+
+        //if (Bridge.player.isAuthorizationSupported && !Bridge.player.isAuthorized)
+        //{
+        //    //откроется новое окно для ввода логина-пароля. если закрыть его, то придет OnAuthorizePlayerCompleted(false)
+        //    var yandexScopes = true; 
+        //    var yandexOptions = new AuthorizeYandexOptions(yandexScopes);
+        //    Bridge.player.Authorize(OnAuthorizePlayerCompleted, yandexOptions);
+        //}
+        //else
+        //{
+        //    //UnityLogger.Log($"Player name {Bridge.player.name}");
+        //    //if(Bridge.player.photos == null)
+        //    //    UnityLogger.Log("Bridge.player.photos == null");
+        //    //if (Bridge.player.photos != null)
+        //    //    UnityLogger.Log($"Bridge.player.photos Count {Bridge.player.photos.Count}");
+        //    //if(Bridge.player.photos.Count > 0)
+        //    //    //возвращает путь к аватарке типа https://games-sdk.yandex.ru/games/api/sdk/v1/player/avatar/0/islands-retina-small
+        //    //    UnityLogger.Log($"Player photo[0] {Bridge.player.photos[0]}");
+
+        TryGetData();
+        //}
+
     }
-    
+
+    public void Auth()
+    {
+        var yandexScopes = true;
+        var yandexOptions = new AuthorizeYandexOptions(yandexScopes);
+        Bridge.player.Authorize(OnAuthorizePlayerCompleted, yandexOptions);
+    }
+
+    private void OnAuthorizePlayerCompleted(bool success)
+    {
+        UnityLogger.Log($"OnAuthorize {success}");
+
+        if (success)
+        {
+            TryGetData();
+        }
+        else
+        {
+           
+        }
+    }
+
+    private void TryGetData()
+    {
+        Bridge.storage.Get("gold", OnStorageGetCompleted);
+    }
+
+    private void OnStorageGetCompleted(bool success, string data)
+    {
+        // Загрузка произошла успешно
+        if (success)
+        {
+            if (data != null)
+            {
+                UnityLogger.Log($"gold: {Bridge.storage.defaultType}");
+                UnityLogger.Log($"gold data: {data}");
+                if(Int32.TryParse(data, out int savedGold))
+                {
+                    GameMaster.Instance.Statistics.Gold = savedGold;
+                    UnityLogger.Log($"gold parsed & runtime:{savedGold}, {GameMaster.Instance.Statistics.Gold}");
+                }
+            }
+            else
+            {
+                UnityLogger.Log($"gold: {Bridge.storage.defaultType}");
+                UnityLogger.Log($"gold: no data");
+            }
+        }
+        else
+        {
+            // Ошибка, что-то пошло не так
+            UnityLogger.Log($"OnStorageGetCompleted: Ошибка, что-то пошло не так");
+        }
+
+        IsReady = true;
+        UnityLogger.Log($"IsReady {IsReady}");
+    }
+
+    //private void OnApplicationFocus(bool focus)
+    //{
+    //    if(!focus)
+    //    {
+    //        UnityLogger.Log($"OnApplicationFocus: {focus}");
+    //        Bridge.storage.Set("gold", GameMaster.Instance.Statistics.Gold, OnStorageSetCompleted);
+    //    }
+    //}
+
+    private void OnStorageSetCompleted(bool success)
+    {
+        Debug.Log($"OnStorageSetCompleted, success: {success}");
+    }
+
     public void ChangeState(GameState state)
     {
         if (CurrentGameState == state)
--- mobius_bike/Assets/UI/TopPanel.cs
+++ mobius_bike/Assets/UI/TopPanel.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections;
+﻿using InstantGamesBridge;
+using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.UI;
 
 public class TopPanel : MonoBehaviour
@@ -9,7 +11,11 @@ public class TopPanel : MonoBehaviour
     [SerializeField] private TextMeshProUGUI _amount;
     [SerializeField] private TextMeshProUGUI _gemsAmount;
     [SerializeField] private Image[] _lifes;
-    
+    [SerializeField] private TextMeshProUGUI _name;
+    [SerializeField] private TextMeshProUGUI _id;
+    [SerializeField] private Image _photo;
+    [SerializeField] private GameObject _authBtn;
+
     private void OnEnable()
     {
         //юнити не гарантирует порядок выполнения методов лайфсайкла,
@@ -32,11 +38,40 @@ public class TopPanel : MonoBehaviour
     private void UpdateScreen()
     {
         _amount.SetText("{0}", GameMaster.Instance.Statistics.Gold);
-        _gemsAmount.SetText("{0}", GameMaster.Instance.Statistics.Gems);
+        //_gemsAmount.SetText("{0}", GameMaster.Instance.Statistics.Gems);
 
         for (int i = 0; i < _lifes.Length; i++)
         {
             _lifes[i].fillAmount = i <= GameMaster.Instance.Statistics.Lifes - 1 ? 1 : 0;
-        } 
+        }
+
+        if(Bridge.player.isAuthorized)
+        {
+            _name.text = $"{ Bridge.player.name }";
+            _id.text = $"{ Bridge.player.id}";
+
+            if (Bridge.player.photos.Count > 0)
+            {
+                StartCoroutine(LoadPhoto(Bridge.player.photos[0]));
+            }
+
+            _authBtn.SetActive(false);
+        }
+        else
+        {
+            _authBtn.SetActive(true);
+        }
     }
+        private IEnumerator LoadPhoto(string url)
+        {
+            var request = UnityWebRequestTexture.GetTexture(url);
+            yield return request.SendWebRequest();
+
+            if (request.result == UnityWebRequest.Result.Success)
+            {
+                var texture = ((DownloadHandlerTexture)request.downloadHandler).texture;
+                var sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
+                _photo.sprite = sprite;
+            }
+        }
 }
